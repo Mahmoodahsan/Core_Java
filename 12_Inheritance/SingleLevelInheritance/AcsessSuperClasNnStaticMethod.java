6f20a5a2aaa5963254f74bc3b6f8dc9bd8a55cad
mahmoodahsan <mahmoodahsan265@gmail.com>
2025-08-12 23:40:14 +0530

    inherit....

---
+++ 12_Inheritance/SingleLevelInheritance/AcsessSuperClasNnStaticMethod.java
@@ -0,0 +1,29 @@
+package single_level_inheritance;
+
+ class Alpha{
+	public String toString() {
+		return "Alpha";
+	}
+}
+ class Beta extends Alpha{
+		public String toString() {
+			System.out.println(super.toString());//Alpha
+			return "Beta";
+		}
+	}
+ class Gamma extends Beta{
+		public String toString() {
+			System.out.println(super.toString());//Beta
+			return "Gamma";
+		}
+	}
+ 
+public class AcsessSuperClasNnStaticMethod {
+
+	public static void main(String[] args) {
+		
+    Gamma g=new Gamma();
+    System.out.println(g);//Gamma
+	}
+
+}
