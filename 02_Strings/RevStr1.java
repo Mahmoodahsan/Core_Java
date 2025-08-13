fda8855ac5814e41bc81af22f8f86aca0d6c3fb7
mahmoodahsan <mahmoodahsan265@gmail.com>
2025-08-13 21:51:38 +0530

    str

--- 02_Strings/RevStr1.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        StringBuffer str=new StringBuffer("mahmood");
-        System.out.print("reverse of string MAHMOOD:");
-        System.out.println(str.reverse());
-        System.out.println("*********END" +
-                "*************");
-    }
-}
