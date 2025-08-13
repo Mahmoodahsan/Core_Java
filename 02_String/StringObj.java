344848fb1ce62665da388f5153be3bd3681412d8
mahmoodahsan <mahmoodahsan265@gmail.com>
2025-08-13 10:02:23 +0530

    string

---
+++ 02_String/StringObj.java
@@ -0,0 +1,20 @@
+package StringLiterals;
+
+public class StringObj {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         //By using string literal
+		String s1="india";
+		System.out.println(s1);
+		//By using new keyword
+		String str2=new String("Hyderabad");
+		System.out.println(str2);
+		//By using Character array[old tech..]
+		char []ch= {'m','k','s'};
+		System.out.println(ch);
+		
+		System.out.println(40+40+"logic"+40+40);
+	}
+
+}
