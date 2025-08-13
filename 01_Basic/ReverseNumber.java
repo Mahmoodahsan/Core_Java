c6635d651a0d8eb04007940b438f4e1ce67fbfe0
mahmoodahsan <mahmoodahsan265@gmail.com>
2025-08-13 21:15:00 +0530

    basic

--- 01_Basic/ReverseNumber.java
+++ 01_Basic/ReverseNumber.java
@@ -1,13 +1,12 @@
-package basic;
-class ReverseNumber {
-  public static void main(String args[]) {
-    int num = 234;
-    int remainder, result = 0;
-    while (num > 0) {
-      remainder = num % 10;
-      result = result * 10 + remainder;
-      num = num / 10;
-    }
-    System.out.println("Reverse	number	is :" + result);
-  }
+class	ReverseNumber{
+    public	static	void	main(String	args[]){	
+   int	num	=234;
+    int	remainder, result=0;	
+    while(num>0){	
+     remainder	=	num%10;
+    result	=	result	*	10	+	remainder;
+    num	=	num/10;
+               }
+    System.out.println("Reverse	number	is :"+result);
+   }
 }
