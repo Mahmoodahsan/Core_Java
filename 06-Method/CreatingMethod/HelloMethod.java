fbddfd011de5d09d9a171359e3532bedec9c1ce6
mahmoodahsan <mahmoodahsan265@gmail.com>
2025-08-15 16:35:54 +0530

    l

--- 06-Method/CreatingMethod/HelloMethod.java
+++
@@ -1,8 +0,0 @@
-public class HelloMethod {
-    public static void sayHello() {
-        System.out.println("Hello, World!");
-    }
-    public static void main(String[] args) {
-        sayHello();
-    }
-}
