4b92864202d71499197fa32f4305c465639765ec
mahmoodahsan <mahmoodahsan265@gmail.com>
2025-08-15 17:47:23 +0530

    l

--- 09_ObjectAsReference/First.java
+++ 09_ObjectAsReference/First.java
@@ -1,23 +1,19 @@
 public class First {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
- Product p1=new Product();
- System.out.println(p1);
-//  p1=new Product();
-//  System.out.println(p1);
-  Product p2=new Product(p1);
-  System.out.println(p2);
-	}
-
+    public static void main(String[] args) {
+        Product p1 = new Product();
+        System.out.println(p1);
+        Product p2 = new Product(p1);
+        System.out.println(p2);
+    }
 }
-class Product{
-	int a;
-	int b;
-	public Product(){
-		System.out.println("hello p");
-	}
-	public Product(Product p1){
-		System.out.println("hello");
-	}
+
+class Product {
+    int a;
+    int b;
+    public Product() {
+        System.out.println("hello p");
+    }
+    public Product(Product p1) {
+        System.out.println("hello");
+    }
 }
