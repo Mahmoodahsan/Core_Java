a4a0915fbab4249d586f73fd7ed544b08eda2182
mahmoodahsan <mahmoodahsan265@gmail.com>
2025-08-15 16:34:49 +0530

    methods

---
+++ 06_Methods/ReturnValue/ReturnName.java
@@ -0,0 +1,8 @@
+public class ReturnName {
+    public static String getName() {
+        return "Mahmood";
+    }
+    public static void main(String[] args) {
+        System.out.println("Name: " + getName());
+    }
+}
