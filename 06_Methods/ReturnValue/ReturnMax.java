1379eda22772b0f5b68699f3c20ea11531fdfdd3
mahmoodahsan <mahmoodahsan265@gmail.com>
2025-08-18 09:29:44 +0530

    l

--- 06_Methods/ReturnValue/ReturnMax.java
+++
@@ -1,8 +0,0 @@
-public class ReturnMax {
-    public static int max(int a, int b) {
-        return (a > b) ? a : b;
-    }
-    public static void main(String[] args) {
-        System.out.println("Max: " + max(12, 25));
-    }
-}
--- 06_Methods/ReturnValue/ReturnName.java
+++
@@ -1,8 +0,0 @@
-public class ReturnName {
-    public static String getName() {
-        return "Mahmood";
-    }
-    public static void main(String[] args) {
-        System.out.println("Name: " + getName());
-    }
-}
